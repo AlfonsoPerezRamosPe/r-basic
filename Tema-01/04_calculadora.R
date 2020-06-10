88cdfd9eaa08ba4eda58dfccb5629c0fed174e7e
AlfonsoPe <61558899+AlfonsoPerez@users.noreply.github.com>
2020-06-10 01:02:30 -0500

    Update 04_calculadora.R

--- Tema-01/04_calculadora.R
+++ Tema-01/04_calculadora.R
@@ -4,4 +4,121 @@
 2^-4
 725%/%7
 2^pi
-s
+factorial(6)
+choose(5,2)
+4^log(6,4)
+10^log(100,10)
+sin(30)
+tan(pi/4)
+sin(pi/6)
+tan(pi/4)
+tan(0)
+?sin
+sinpi(1/6)
+asin(0.5)*180/pi
+pi/6
+x = seq(0,2*pi,0.1)
+x
+
+print(1.255555,2)
+round(1.56585,2)
+sqrt(8)
+print(sqrt(2),8)
+round(sqrt(2),8)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+(sqrt(2)^2)-2
+2^50
+print(2^50,10)
+
+x = 4
+y = 5
+suma = function(a,b){a+b}
+suma(x,y)
+raiz = function(a){sqrt(a)}
+raiz(x)
+
+x = (pi^2)/2
+y <- cos(pi/4)
+sin(pi/4)+cos(pi/4) -> z
+z
+y
+Hola <- 2
+hoLa <- 5
+Hola + hoLa
+sqrt(4)
+pi.4 = 4*pi
+pi.4
+x=2
+x=x^2
+x
+
+x = 4
+y = 5
+suma = function(a,b){a+b}
+suma(x,y)
+raiz = function(a){sqrt(a)}
+raiz(x)
+cuadrado = function(a,b){a^2}
+cuadrado(y)
+f = function(x){x^3-(3^x)*sin(x)}
+f(4)
+exp(2)
+2.718282^2
+y = rep(1:5)
+y
+y =rep(1)
+?rep
+y <- rep(1:20)
+y
+exp(y)
+exp(y[4])
+seq(1,10,3)
+x=seq(-4,10, by=0.1)
+x
+plot(x,exp(x),typ="l",col="blue")
+
+graf =function(a){
+  plot(a,exp(a),typ="l",col="blue")
+}
+x <- seq(-2,10, by=0.1)
+graf(x)
+
+graf2 = function(a){
+  plot(a,a^2,typ="l",col="red")
+  
+}
+x <- seq(-4,10, by=1)
+graf2(x)
+suma1 = function(x){x+1}
+print(suma1(4),2)
+suma5=function(x){
+  x=suma1(x)
+  x=suma1(x)
+  x=suma1(x)
+  x=suma1(x)
+  suma1(x)
+
+}
+suma5(2)
+ls()
+rm(pi.4)
+ls()
+rm(list = ls())
+ls()
+opera=function(x,y){
+  print("operaciones")
+  print(x+y)
+  print(x-y)
+  print(paste(sprintf("%i - %i = ",x,y),x-y))
+  print(paste(sprintf("%i - %i = ",y,x),y-x))
+  print(x*y)
+  print(x%/%y)
+  print(paste(sprintf("%i / %i = ",x,y),x%/%y))
+  print(x%%y)
+}
+
+opera(10,3)
+
